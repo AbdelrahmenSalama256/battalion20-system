77c9638a4eda651455a679b07349b35202c370a1
AbdelrahmenSalama256 <abdosalama25802030@gmail.com>
2026-06-09 05:37:37 +0300

    fix: strip netlify functions prefix from request path in serverless handler

--- dashboard/netlify/functions/api.js
+++ dashboard/netlify/functions/api.js
@@ -1,3 +1,8 @@
 const serverless = require('serverless-http');
 const app = require('../../api/src/app');
-exports.handler = serverless(app);
+
+exports.handler = serverless(app, {
+  request: function (request, event) {
+    request.url = event.path.replace('/.netlify/functions/api', '');
+  }
+});
